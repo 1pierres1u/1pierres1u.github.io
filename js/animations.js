910afca251bca4bbac33b3a9431175ad3cf2b70b
Pierre S Louis-Jacques <1pierres1u@gmail.com>
2020-12-26 20:28:14 -0500

    Replaced the image of the index page.Add a red bar at the bottom of the menu links. Change the color of the background and text. Added fillers and a css grid wrapper for the pages about.html and contact.html

--- js/animations.js
+++ js/animations.js
@@ -1,24 +1,55 @@
-let animation = anime({
-   targets:'div#home_content > img',
-   rotateX:'360',
+let animation_1 = anime({
+   targets:'div#home_content > img#bar_1',
+   rotateY:'34',
+   easing:'linear',
+   loop:true,
+});
+let animation_2 = anime({
+   targets:'div#home_content > img#bar_2',
+   rotateY:'55',
+   easing:'linear',
+   loop:true,
+});
+let animation_3 = anime({
+   targets:'div#home_content > img#bar_3',
+   rotateY:'89',
+   easing:'linear',
+   loop:true,
+});
+let animation_4 = anime({
+   targets:'div#home_content > img#bar_4',
+   rotateY:'144',
+   easing:'linear',
+   loop:true,
+});
+let animation_5 = anime({
+   targets:'div#home_content > img#bar_5',
+   rotateY:'233',
    easing:'linear',
    loop:true,
 });
 
+var bottomLine = "2px solid red" ;
+
 if(document.title=="Home")
 {
-  $("ul a").eq(0).css("color","red");
+  $("ul li ").eq(0).css("padding","3px");
+  $("ul li ").eq(0).css("border-bottom",bottomLine);
 }
 else if(document.title=="Projects")
 {
-  $("ul a").eq(1).css("color", "red");
+  $("ul li ").eq(1).css("padding","3px");
+  $("ul li ").eq(1).css("border-bottom",bottomLine);
 }
 else if(document.title=="About")
 {
- $("ul a").eq(2).css("color","red");
+  $("ul li ").eq(2).css("padding","3px");
+  $("ul li ").eq(2).css("border-bottom",bottomLine);
 }
 else if(document.title=="Contact")
 {
-  $("ul a").eq(3).css("color","red");
+  $("ul li ").eq(3).css("padding","3px");
+  $("ul li ").eq(3).css("border-bottom",bottomLine);
 }
+
 
